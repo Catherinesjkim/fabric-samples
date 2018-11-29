ca820fa3ee15127167e6d17174094ce8a18eff12
Matt Roberts <code@rbrts.uk>
2018-11-29 17:33:46 +0000

    Upgrade to Cicero 0.9.8
    Signed-off-by: Matt Roberts <code@rbrts.uk>

--- cicero/startFabric.sh
+++ cicero/startFabric.sh
@@ -10,11 +10,8 @@ set -e
 # don't rewrite paths for Windows Git Bash users
 export MSYS_NO_PATHCONV=1
 starttime=$(date +%s)
-LANGUAGE=${1:-"golang"}
-CC_SRC_PATH=github.com/cicero/go
-if [ "$LANGUAGE" = "node" -o "$LANGUAGE" = "NODE" ]; then
-	CC_SRC_PATH=/opt/gopath/src/github.com/cicero/node
-fi
+LANGUAGE="node"
+CC_SRC_PATH=/opt/gopath/src/github.com/cicero/node
 
 # increase the timeouts
 export CORE_CHAINCODE_DEPLOYTIMEOUT=300s
@@ -39,7 +36,5 @@ sleep 10
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp" cli peer chaincode invoke -o orderer.example.com:7050 -C mychannel -n cicero -c '{"function":"initLedger","Args":[""]}'
 
 printf "\nTotal setup execution time : $(($(date +%s) - starttime)) secs ...\n\n\n"
-printf "Start by installing required packages run 'npm install'\n"
-printf "Then run 'node enrollAdmin.js', then 'node registerUser'\n\n"
-printf "The 'node invoke.js' will fail until it has been updated with valid arguments\n"
-printf "The 'node query.js' may be run at anytime once the user has been registered\n\n"
+printf "Start by installing required packages run \n\tnpm install\n"
+printf "Then run: \n\tnode enrollAdmin.js\n\tnode registerUser.js\n\tnode deploy.js\n"
